1ad49ea0555f12e909a73758ee90d45d84991609
unknown <bouaoun.toufik@gmail.com>
2021-05-04 20:45:16 -0400

    Updates

---
+++ precache-manifest.4c60c3bf737fc5800b8f513b1c49fbe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16bed77840a88497e08972f923c38b8",
+    "url": "/burgerappdep/index.html"
+  },
+  {
+    "revision": "2264362b46cccba8a654",
+    "url": "/burgerappdep/static/css/main.5f9b1604.chunk.css"
+  },
+  {
+    "revision": "8bb707117a83c985fb74",
+    "url": "/burgerappdep/static/js/2.adfa3592.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/burgerappdep/static/js/2.adfa3592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2264362b46cccba8a654",
+    "url": "/burgerappdep/static/js/main.f4a78643.chunk.js"
+  },
+  {
+    "revision": "b7c5bddf6b6f5b2112cd",
+    "url": "/burgerappdep/static/js/runtime-main.e7c5dec1.js"
+  },
+  {
+    "revision": "247373de39f999e27e65805f83d3b5cc",
+    "url": "/burgerappdep/static/media/background.247373de.jpg"
+  }
+]);
